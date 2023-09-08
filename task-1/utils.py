f99e28617adb70296b40beab794845dfcfeb13a6
Pansaar Kerdpol <pansaar@gmail.com>
2023-09-08 16:19:42 +0700

    Task-1 Completion

--- task-1/utils.py
+++ task-1/utils.py
@@ -1,13 +1,12 @@
 from collections import deque
 
-# a node = a tuple of 5 members
+# A node = a tuple of 3 members
 def create_node(state, parent, action, path_cost, depth):
     return (state, parent, action, path_cost, depth)
-
 # Trace back from a goal node, n, to the initial node
 # to generate a solution
 def print_solution(n):
-    r = deque() # double-end queue
+    r = deque()  # double-ended queue
     while n is not None:
         r.appendleft(n[0])
         n = n[1]
